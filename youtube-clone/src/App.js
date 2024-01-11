41ca8450014b310097a28e64242f5495271b8615
Kamogelo-MM <kammolawa@gmail.com>
2024-01-12 01:34:17 +0200

    update
    created new src folder.
    created components folder
    kind of finished navbar
    using mui

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+  SearchBar,
+} from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/chanel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerms" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
